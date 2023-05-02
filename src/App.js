e34c93fd72362a0a8f660e4c58ee24c16b24d52b
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-02 16:55:48 +0500

    client updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+  const [msg, setMsg] = useState()
+  const [msgArr, setMsgArr] = useState([])
+
+  useEffect(() => {
+  }, [msgArr])
+
+  const onSend = async (e) => {
+    e.preventDefault();
+
+    console.log('message sent', msg)
+    msgArr.push(msg + ' l')
+    setMsgArr(...[msgArr])
+
+    let respond = await axios.post('http://localhost:3001/userMsg',
+      {
+        "to_number": '923232846250',
+        "message": msg,
+        "type": 'text',
+        "prev": msgArr[msgArr.length - 2]
+      }
+    )
+    console.log('respond', respond)
+    msgArr.push(respond.data.reply + ' r')
+    setMsgArr(...[msgArr])
+    setMsg('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <form>
+        <h1>Talk to ChatBot!</h1>
+        <div
+          className='float'
         >
-          Learn React
-        </a>
-      </header>
+          {msgArr.map((v, i) => <div
+            className={`${v[v.length - 1] === 'l' ? 'left floa' : 'right'}`}
+            key={i}
+          >
+            {v.slice(0, v.length - 1)}
+          </div>)}
+        </div>
+        <div className="d-flex align-items-center mt-3">
+          <div className="form-group">
+            <label htmlFor="userMessage">Type Your Message</label>
+            <input type="text" className="form-control" id="userMessage" value={msg} placeholder="Your Message Here" onChange={e => setMsg(e?.target?.value)} />
+          </div>
+          <button type="submit" className="btn btn-primary h-25 ml-2 mt-3" onClick={(e) => onSend(e)}>Send</button>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
